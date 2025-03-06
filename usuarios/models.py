4c07bde259b9b98af7b6abb5684e70972de0d77a
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-03-05 23:23:19 -0500

    imagen de perfil

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+import hashlib
+
 class Usuario(AbstractUser):
     username = None
     nombre = models.CharField(max_length=100)
@@ -10,10 +12,13 @@ class Usuario(AbstractUser):
     telefono = models.CharField(max_length=15, blank=True, null=True)
     fecha_nacimiento = models.DateField(blank=True, null=True)
 
-    # Cambia el username por el email
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nombre']
 
     def __str__(self):
         return self.email
+
+    def get_gravatar_url(self, size=100):
+        email_hash = hashlib.md5(self.email.strip().lower().encode('utf-8')).hexdigest()
+        return f"https://www.gravatar.com/avatar/{email_hash}?s={size}&d=identicon"
 
