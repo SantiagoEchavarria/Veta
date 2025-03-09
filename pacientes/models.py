9c66fcaa6b5a01408b5c778b54ca6afa5481ff4c
Bryan <bryyandaniiel@gmail.com>
2025-03-09 12:55:17 -0500

    Tipo de diabetes en paciente con forma de enum

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,10 +1,21 @@
 from django.conf import settings
 from django.db import models
 from usuarios.models import Usuario 
+
+class TipoDiabetes(models.TextChoices):
+    TIPO1= 'Tipo1','Tipo1'
+    TIPO2= 'Tipo2','Tipo2'
+    GESTACIONAL = 'Gestacional', 'Gestacional'
+    OTRO = 'Otro', 'Otro'
+
 class Paciente(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=150, editable=False)
-    tipo_diabetes = models.CharField(max_length=50)
+    tipo_diabetes=models.CharField(
+        max_length=50,
+        choices=TipoDiabetes.choices,
+        default=TipoDiabetes.TIPO2
+    )
 
     def save(self, *args, **kwargs):
         self.nombre = self.usuario.nombre
