bde74991e762ac164f800dec3c73304c21c11e2d
Bryan <bryyandaniiel@gmail.com>
2025-03-09 13:52:40 -0500

    Detalles del paciente

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import Paciente
 
-# Create your views here.
+@login_required
+def mi_detalle_paciente(request):
+    try:
+        paciente = Paciente.objects.get(usuario=request.user)
+    except Paciente.DoesNotExist:
+        # Crear automáticamente el perfil de paciente para el usuario
+        paciente = Paciente.objects.create(usuario=request.user)
+        # Opcionalmente, redirigir a una página para que el usuario complete su información
+        # return redirect('completar_perfil')
+    
+    context = {'paciente': paciente}
+    return render(request, 'pacientes_detalles.html', context)
--- veta/urls.py
+++ veta/urls.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from django.urls import path
-from usuarios import views
+from usuarios import views as views_usuarios
 from medicaciones import views as views_medicaciones
 from django.contrib.auth import views as auth_views
+from pacientes import views as views_pacientes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #urls de Usuarios:
-    path('', views.inicio, name='inicio'),
-    path('crear_seccion/', views.crearSeccion, name='crear_seccion'),
-    path('iniciar_seccion/', views.iniciarSeccion, name='iniciar_seccion'),
-    path('cerrar_seccion/', views.cerrarSeccion, name='cerrar_seccion'),
-    path('editar_seccion/', views.editarSeccion, name='editar_seccion'),
+    # URLs de Usuarios:
+    path('', views_usuarios.inicio, name='inicio'),
+    path('crear_seccion/', views_usuarios.crearSeccion, name='crear_seccion'),
+    path('iniciar_seccion/', views_usuarios.iniciarSeccion, name='iniciar_seccion'),
+    path('cerrar_seccion/', views_usuarios.cerrarSeccion, name='cerrar_seccion'),
+    path('editar_seccion/', views_usuarios.editarSeccion, name='editar_seccion'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
-    #Urls de medicaciones:
+    # URLs de Medicaciones:
     path('medicaciones/crear/', views_medicaciones.crear_medicacion, name='crear_medicacion'),
-
-    ]
+    
+    # URL para ver el detalle del paciente autenticado:
+    path('mi_paciente/', views_pacientes.mi_detalle_paciente, name='mi_detalle_paciente'),
+]
