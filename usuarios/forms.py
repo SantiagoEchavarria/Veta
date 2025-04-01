9a8c7682ff4cd6a688ff69bb7cd053730a5a1428
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-03-31 21:08:04 -0500

    Mejoras a editar perfil

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm,  PasswordChangeForm
 from .models import Usuario
 from django.contrib.auth import password_validation
-
+from django.core.exceptions import ValidationError
+from datetime import date
 
 class UsuarioCreationForm(UserCreationForm):
     class Meta:
@@ -12,7 +13,35 @@ class UsuarioCreationForm(UserCreationForm):
 class UsuarioUpdateForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ['nombre', 'email', 'telefono', 'fecha_nacimiento'] 
+        fields = ['nombre', 'email', 'telefono', 'fecha_nacimiento']
+    
+    # Validación para el campo nombre
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if not nombre.isalpha():
+            raise ValidationError("El nombre solo puede contener letras.")
+        return nombre
+    
+    # Validación para el campo email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not '@' in email:
+            raise ValidationError("Por favor ingrese un correo electrónico válido.")
+        return email
+    
+    # Validación para el campo teléfono
+    def clean_telefono(self):
+        telefono = self.cleaned_data.get('telefono')
+        if len(telefono) < 10:
+            raise ValidationError("El teléfono debe tener al menos 10 dígitos.")
+        return telefono
+    
+    # Validación para el campo fecha_nacimiento
+    def clean_fecha_nacimiento(self):
+        fecha_nacimiento = self.cleaned_data.get('fecha_nacimiento')
+        if fecha_nacimiento >= date.today():
+            raise ValidationError("La fecha de nacimiento no puede ser futura.")
+        return fecha_nacimiento
 
 class UsuarioContrasenaForm(PasswordChangeForm):
     old_password = forms.CharField(
